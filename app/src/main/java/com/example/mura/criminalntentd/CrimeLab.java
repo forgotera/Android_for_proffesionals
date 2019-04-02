ed57c91560cd3105f0a1fc77ca6dc50515570f46
Gaipov Murad <m.gaipov@mail.ru>
2019-04-02 20:28:33 +0300

    added toolbar

--- app/src/main/java/com/example/mura/criminalntentd/CrimeLab.java
+++ app/src/main/java/com/example/mura/criminalntentd/CrimeLab.java
@@ -20,12 +20,7 @@ public class CrimeLab {
 
     private CrimeLab(Context context){
         mCrimes = new ArrayList<>();
-        for(int i = 0; i < 100;i++){
-            Crime crime = new Crime();
-            crime.setmTitle("Crime #"+i);
-            crime.setmSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+
     }
 
     public List<Crime> getmCrimes(){
@@ -40,6 +35,10 @@ public class CrimeLab {
             }
         }
         return null;
+    }
+
+    public void addCrime(Crime c){
+        mCrimes.add(c);
     }
 
 }
--- app/src/main/java/com/example/mura/criminalntentd/CrimeListFragment.java
+++ app/src/main/java/com/example/mura/criminalntentd/CrimeListFragment.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -21,6 +25,8 @@ public class CrimeListFragment extends Fragment {
  private CrimeAdapter mAdapter;
  private ImageView mSolvedImageView;
  private int position;
+ private boolean mSubtitleVisible;
+ public static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,8 +35,18 @@ public class CrimeListFragment extends Fragment {
    mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
    mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+   if(savedInstanceState != null){
+       mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+   }
+
    updateUI();
    return view;
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState){
+      super.onSaveInstanceState(outState);
+      outState.putBoolean(SAVED_SUBTITLE_VISIBLE,mSubtitleVisible);
   }
 
 
@@ -44,12 +60,15 @@ public class CrimeListFragment extends Fragment {
    }else{
        mAdapter.notifyItemChanged(position);
    }
+
+   updateSubtitle();
  }
 
     @Override
     public void onResume(){
         super.onResume();
         updateUI();
+
     }
 
  //Holder созадет элементы textView и добавляет их на экран
@@ -111,6 +130,59 @@ public class CrimeListFragment extends Fragment {
     return mCrimes.size();
    }
 
+  }
+
+  @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+      super.onCreateOptionsMenu(menu,inflater);
+      inflater.inflate(R.menu.fragment_crime_list,menu);
+
+      //повторное создание создание элементов при нажатии
+      MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
+      if(mSubtitleVisible){
+          subtitleItem.setTitle(R.string.hide_subtitle);
+      }else{
+          subtitleItem.setTitle(R.string.show_subtitle);
+      }
+  }
+
+  @Override
+    public void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setHasOptionsMenu(true);
+  }
+
+  //какая кнопка нажата
+  @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+      switch (item.getItemId()){
+          case R.id.new_crime:
+              Crime crime = new Crime();
+              CrimeLab.get(getActivity()).addCrime(crime);
+              Intent intent = CrimePagerActivity.newIntent(getActivity(),crime.getmId());
+              startActivity(intent);
+              return true;
+          case R.id.show_subtitle:
+              mSubtitleVisible = !mSubtitleVisible;
+              getActivity().invalidateOptionsMenu();
+              updateSubtitle();
+              return true;
+              default:
+                  return super.onOptionsItemSelected(item);
+      }
+  }
+
+  //подзаголовок
+  private void updateSubtitle(){
+      CrimeLab crimeLab = CrimeLab.get(getActivity());
+      int crimeCount = crimeLab.getmCrimes().size();
+      String subtitle = getString(R.string.subtitle_format,crimeCount);
+
+      if(!mSubtitleVisible){
+          subtitle = null;
+      }
+      AppCompatActivity activity = (AppCompatActivity) getActivity();
+      activity.getSupportActionBar().setSubtitle(subtitle);
   }
 
 }
